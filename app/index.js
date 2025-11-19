de19182a675a7e4ac505e22ef33c1ad9e16c78f3
Somanos Sar <somanos@drumee.com>
2025-11-19 10:00:25 +0700

    Chqnged loqder

--- app/index.js
+++ app/index.js
@@ -1,9 +1,4 @@
 
-/**
- * Load Drumee rendering engine (LETC)
- * Work from electron
- * @param {*} e 
- */
 async function start(e) {
   document.removeEventListener('drumee:ready', start);
   let el = document.getElementById("loader-spinner");
@@ -11,12 +6,6 @@ async function start(e) {
   let kind = 'onboarding';
   console.log(`Loading onboarding, kind=${kind}`);
   Kind.registerAddons(require("./seeds"));
-  Kind.waitFor(kind).then(async (k) => {
-    console.log(`Startging kind=${kind}!`);
-    uiRouter.ensurePart(_a.body).then((p) => {
-      p.feed({ kind });
-    })
-  })
 }
 
 if (document.readyState == 'complete') {
